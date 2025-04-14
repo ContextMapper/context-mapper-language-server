a0e7a8c5a22d416169785f5992dc8d8e82282a4f
Lukas Streckeisen <lukas@streckeisen.dev>
2025-04-14 17:12:57 +0200

    complete bounded context semantic token tests

--- test/semnantictokens/BoundedContextSemanticTokens.test.ts
+++ test/semnantictokens/BoundedContextSemanticTokens.test.ts
@@ -1,11 +1,15 @@
-import { afterEach, beforeAll, describe, expect, test } from 'vitest'
+import { afterEach, beforeAll, describe, test } from 'vitest'
 import { createContextMapperDslServices } from '../../src/language/context-mapper-dsl-module.js'
 import { EmptyFileSystem, type LangiumDocument } from 'langium'
 import { SemanticTokenProvider } from 'langium/lsp'
 import { clearDocuments, parseHelper } from 'langium/test'
 import { ContextMappingModel } from '../../src/language/generated/ast.js'
-import { SemanticTokensParams } from 'vscode-languageserver'
-import { assertSemanticToken, TOKEN_DATA_LENGTH } from './SemanticTokenTestHelper.js'
+import {
+  assertSemanticToken,
+  assertSemanticTokenLength, createSemanticTokenParams,
+  extractSemanticTokens
+} from './SemanticTokenTestHelper.js'
+import { SemanticTokens } from 'vscode-languageserver-types'
 
 let services: ReturnType<typeof createContextMapperDslServices>
 let parse: ReturnType<typeof parseHelper<ContextMappingModel>>
@@ -23,37 +27,84 @@ afterEach(async () => {
 })
 
 describe('BoundedContext semantic token test', () => {
-  test('Test bounded context without body', async () => {
-    document = await parse('BoundedContext FirstContext')
-    const params: SemanticTokensParams = {
-      textDocument: {
-        uri: document.uri.path
-      }
-    }
+  test('check bounded context without body', async () => {
+    document = await parse('BoundedContext TestContext')
+    const params = createSemanticTokenParams(document)
     const result = await semanticTokenProvider.semanticHighlight(document, params)
-    const expectedNumberOfTokens = 2
-    expect(result).not.toBeNull()
-    expect(expectedNumberOfTokens * TOKEN_DATA_LENGTH).toEqual(result.data.length)
+    assertEmptyBoundedContext(result)
+  })
 
-    const firstToken = result.data.slice(0, 5)
-    const secondToken = result.data.slice(5, 10)
+  test('check bounded context with empty body', async () => {
+    document = await parse('BoundedContext TestContext {}')
+    const params = createSemanticTokenParams(document)
+    const result = await semanticTokenProvider.semanticHighlight(document, params)
+    assertEmptyBoundedContext(result)
+  })
 
-    assertSemanticToken(
-      firstToken,
-      0,
-      0,
-      14,
-      semanticTokenProvider.tokenTypes.keyword,
-      0
-    )
+  test('check bounded context with member attributes', async () => {
+    document = await parse(`
+    BoundedContext TestContext {
+      type = UNDEFINED
+      implementationTechnology = "java"
+      responsibilities = "resp1", "resp2"
+      businessModel = "model"
+      domainVisionStatement = "Test"
+      knowledgeLevel = CONCRETE
+      evolution GENESIS
+    }
+    `)
+    const params = createSemanticTokenParams(document)
+    const result = await semanticTokenProvider.semanticHighlight(document, params)
 
-    assertSemanticToken(
-      secondToken,
-      0,
-      15,
-      12,
-      semanticTokenProvider.tokenTypes.type,
-      semanticTokenProvider.tokenModifiers.declaration
-    )
+    const expectedNumberOfTokens = 17
+    assertSemanticTokenLength(result, expectedNumberOfTokens)
+    const tokens = extractSemanticTokens(result, expectedNumberOfTokens)
+
+    assertSemanticToken(tokens[2], 1, 6, 4, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[3], 0, 7, 9, semanticTokenProvider.tokenTypes.property, 0)
+
+    assertSemanticToken(tokens[4], 1, 6, 24, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[5], 0, 27, 6, semanticTokenProvider.tokenTypes.string, 0)
+
+    assertSemanticToken(tokens[6], 1, 6, 16, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[7], 0, 19, 7, semanticTokenProvider.tokenTypes.string, 0)
+    assertSemanticToken(tokens[8], 0, 9, 7, semanticTokenProvider.tokenTypes.string, 0)
+
+    assertSemanticToken(tokens[9], 1, 6, 13, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[10], 0, 16, 7, semanticTokenProvider.tokenTypes.string, 0)
+
+    assertSemanticToken(tokens[11], 1, 6, 21, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[12], 0, 24, 6, semanticTokenProvider.tokenTypes.string, 0)
+
+    assertSemanticToken(tokens[13], 1, 6, 14, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[14], 0, 17, 8, semanticTokenProvider.tokenTypes.property, 0)
+
+    assertSemanticToken(tokens[15], 1, 6, 9, semanticTokenProvider.tokenTypes.keyword, 0)
+    assertSemanticToken(tokens[16], 0, 10, 7, semanticTokenProvider.tokenTypes.property, 0)
   })
 })
+
+function assertEmptyBoundedContext (result: SemanticTokens) {
+  const expectedNumberOfTokens = 2
+  assertSemanticTokenLength(result, expectedNumberOfTokens)
+
+  const tokens = extractSemanticTokens(result, expectedNumberOfTokens)
+
+  assertSemanticToken(
+    tokens[0],
+    0,
+    0,
+    14,
+    semanticTokenProvider.tokenTypes.keyword,
+    0
+  )
+
+  assertSemanticToken(
+    tokens[1],
+    0,
+    15,
+    11,
+    semanticTokenProvider.tokenTypes.type,
+    semanticTokenProvider.tokenModifiers.declaration
+  )
+}
--- test/semnantictokens/SemanticTokenTestHelper.ts
+++ test/semnantictokens/SemanticTokenTestHelper.ts
@@ -1,10 +1,23 @@
 import { expect } from 'vitest'
+import { SemanticTokens } from 'vscode-languageserver-types'
+import { SemanticTokensParams } from 'vscode-languageserver'
+import { LangiumDocument } from 'langium'
+import { ContextMappingModel } from '../../src/language/generated/ast.js'
 
 export const TOKEN_DATA_LENGTH = 5
 
+export function createSemanticTokenParams (document: LangiumDocument<ContextMappingModel>): SemanticTokensParams {
+  return {
+    textDocument: {
+      uri: document.uri.path
+    }
+  }
+}
+
 /**
  * A Semantic Token data array consists of a sequence of integers.
- * One token corresponds to a sequence of 5 integers, representing: startLine, startChar, length, type, modifier
+ * One token corresponds to a sequence of 5 integers, representing: deltaLine, deltaStart, length, type, modifier
+ * Tokens are given in their relative position to each other. deltaLine and deltaStart specifies how many lines & chars the start of the two tokens are apart
  */
 export function assertSemanticToken (token: number[], startLine: number, startCharacter: number, length: number, tokenType: number, tokenModifiers: number) {
   expect(token.length).toEqual(TOKEN_DATA_LENGTH)
@@ -14,4 +27,17 @@ export function assertSemanticToken (token: number[], startLine: number, startCh
   expect(token[2]).toEqual(length)
   expect(token[3]).toEqual(tokenType)
   expect(token[4]).toEqual(tokenModifiers)
+}
+
+export function assertSemanticTokenLength (result: SemanticTokens, expectedNumberOfTokens: number) {
+  expect(result).not.toBeNull()
+  expect(result.data.length).toEqual(expectedNumberOfTokens * TOKEN_DATA_LENGTH)
+}
+
+export function extractSemanticTokens (result: SemanticTokens, expectedNumberOfTokens: number): number[][] {
+  const tokens: number[][] = []
+  for (let i = 0; i < expectedNumberOfTokens; i++) {
+    tokens.push(result.data.slice(i * TOKEN_DATA_LENGTH, (i + 1) * TOKEN_DATA_LENGTH))
+  }
+  return tokens
 }
