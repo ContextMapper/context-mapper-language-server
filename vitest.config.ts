fd944ed1676e0f89841990f6e5d0cf48488f3220
Lukas Streckeisen <lukas@streckeisen.dev>
2025-04-18 09:54:10 +0200

    setup coverage for qodana

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['lcov'],
+      reportsDirectory: '.qodana/code-coverage',
       include: ['src'],
       exclude: ['**/generated']
     },
