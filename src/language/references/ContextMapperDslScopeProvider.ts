80075cebba2bf36a01d1cdb70f8dc16f34cf3933
Lukas Streckeisen <lukas@streckeisen.dev>
2025-04-28 10:50:05 +0200

    add comment explaining necessity of scope provider

--- src/language/references/ContextMapperDslScopeProvider.ts
+++ src/language/references/ContextMapperDslScopeProvider.ts
@@ -8,6 +8,11 @@ import {
 } from '../generated/ast.js'
 
 export class ContextMapperDslScopeProvider extends DefaultScopeProvider {
+  /*
+    Some ContextMapper elements are not defined on the top-level of the document and should still be referencable.
+    Langium assumes nested elements to not be in global scope.
+    Therefore, nested elements are explicitly added to the scope, while for other elements the Langium default behavior applies.
+   */
   override getScope (context: ReferenceInfo): Scope {
     const referenceType = this.reflection.getReferenceType(context)
     const model = AstUtils.getContainerOfType(context.container, isContextMappingModel)!
