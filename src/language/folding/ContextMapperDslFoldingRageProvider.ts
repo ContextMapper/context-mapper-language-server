ec5ffa66e603cca9e7c3f0b2a66fa40d66323fe8
Lukas Streckeisen <lukas@streckeisen.dev>
2025-04-28 13:26:00 +0200

    adjust end position for comment block folding range

--- src/language/folding/ContextMapperDslFoldingRageProvider.ts
+++ src/language/folding/ContextMapperDslFoldingRageProvider.ts
@@ -1,6 +1,6 @@
 import { DefaultFoldingRangeProvider } from 'langium/lsp'
 import { CstNode, LangiumDocument } from 'langium'
-import { FoldingRange } from 'vscode-languageserver-types'
+import { FoldingRange, FoldingRangeKind } from 'vscode-languageserver-types'
 
 export class ContextMapperDslFoldingRangeProvider extends DefaultFoldingRangeProvider {
   // modified version of DefaultFoldingRangeProvider#toFoldingRange
@@ -14,11 +14,25 @@ export class ContextMapperDslFoldingRangeProvider extends DefaultFoldingRangePro
       return undefined
     }
 
-    if (!this.includeLastFoldingLine(node, kind)) { // checks if block is parenthesis/bracket/brace block
+    if (!this.includeLastFoldingLine(node, kind)) { // checks if block is parenthesis/bracket/brace block or comment
       // To prevent something like '...}' in the editor when a code block is collapsed, we want to still show the first line of a code block
-      start = document.textDocument.positionAt(document.textDocument.offsetAt({ line: start.line + 1, character: 0 }) - 1)
-      // As we don't want to hide the end token like 'if { ... --> } <--', we simply select the end of the previous line as the end position
-      end = document.textDocument.positionAt(document.textDocument.offsetAt({ line: end.line, character: 0 }) - 1)
+      start = document.textDocument.positionAt(document.textDocument.offsetAt({
+        line: start.line + 1,
+        character: 0
+      }) - 1)
+
+      let offsetFromEnd
+      if (kind === FoldingRangeKind.Comment) {
+        // So that comments are collapsed like '/*...*/' the end character needs to be at the start of the last line
+        offsetFromEnd = 0
+      } else {
+        // As we don't want to hide the end token like 'if { ... --> } <--', we simply select the end of the previous line as the end position
+        offsetFromEnd = 1
+      }
+      end = document.textDocument.positionAt(document.textDocument.offsetAt({
+        line: end.line,
+        character: 0
+      }) - offsetFromEnd)
     }
     return FoldingRange.create(start.line, end.line, start.character, end.character, kind)
   }
