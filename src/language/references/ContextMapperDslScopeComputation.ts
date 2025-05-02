95e06871865807168def3505c571b36f61e9b5ba
Lukas Streckeisen <lukas@streckeisen.dev>
2025-05-02 13:35:52 +0200

    fix wording

--- src/language/references/ContextMapperDslScopeComputation.ts
+++ src/language/references/ContextMapperDslScopeComputation.ts
@@ -5,7 +5,7 @@ export class ContextMapperDslScopeComputation extends DefaultScopeComputation {
   /*
     For the time being imports aren't supported yet.
     By default, Langium adds top-level elements to the global scope.
-    Without this behavior is wrong and therefore no nodes must be exported here
+    Without imports, this behavior is wrong and therefore no nodes must be exported here.
    */
   override computeExportsForNode (
     parentNode: AstNode,
