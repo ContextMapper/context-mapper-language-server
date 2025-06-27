29a8738132ad46dee389cb63b9666cb7bd6af238
Lukas Streckeisen <lukas@streckeisen.dev>
2025-06-27 10:45:06 +0200

    remove qodana to prepare for repo transfer

--- vitest.config.ts
+++ vitest.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['lcov'],
-      reportsDirectory: '.qodana/code-coverage',
+      reportsDirectory: 'out/code-coverage',
       include: ['src'],
       exclude: ['**/generated']
     },
